b2ae3e0f899d73ce99a7af2dfb6fa1fb03d80027
Grant Ales <grant.ales@yahoo.com>
2021-11-15 16:39:14 -0700

    changes

---
+++ main.js
@@ -0,0 +1,3 @@
+// Have a user log in with a username and password
+// Build a messanger to send encrypted passwords
+// Create a decryption function to decrypt caesar
--- main.py
+++
@@ -1,4 +0,0 @@
-# TODO:
-# Have a user log in with a username and password
-# Build a messanger to send encrypted passwords
-# Create a decryption function to decrypt caesar
